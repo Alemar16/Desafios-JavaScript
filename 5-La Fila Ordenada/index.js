71c18cf6b0f17ffbc1a0bd99fb2aa35aa65fd64c
Armando Martinez <alemar.martinez16@gmail.com>
2022-12-13 23:53:25 -0300

    Ejercicio-La_Fila_ordenada

---
+++ 5-La Fila Ordenada/index.js
@@ -0,0 +1,13 @@
+const arreglo = [];
+
+setInterval(agregarElemento, 4000, () => Math.floor(Math.random() * 100));
+
+function agregarElemento(numeroAleatorio) {
+  const prioridad = numeroAleatorio();
+  //Agregar el elemento prioridad al arreglo mantenido en el orden ascendente
+
+  arreglo.push(prioridad);
+  arreglo.sort((a, b) => a - b);
+}
+
+console.log(arreglo);
